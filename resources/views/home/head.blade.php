32500c2aeb6b82df8a7b05d9da482cf9c554f096
Aziz Bouzir <aziz111134@live.fr>
2024-02-01 21:16:11 +0100

    trying stripe elements

--- resources/views/home/head.blade.php
+++ resources/views/home/head.blade.php
@@ -23,3 +23,4 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.18.0/font/bootstrap-icons.css" rel="stylesheet">
 
+
